683bd52f5805caa2cb095afb868f26f57d00c461
Hitesh Patel <hitesh.patel@crowdstrike.com>
2022-08-29 18:56:42 -0400

    Helpers

--- app/components/product.js
+++ app/components/product.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
-export default class ProductComponent extends Component {}
+export default class ProductComponent extends Component {
+  productImage = this.args.productInfo.colors[0].image;
+}
